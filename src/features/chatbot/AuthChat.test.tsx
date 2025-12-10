9b826a15d75fadacb7f8005adf75fc945ce0fa78
jscottvogel <j_scott_vogel@yahoo.com>
2025-12-10 13:08:25 -0600

    feat: add unit tests for AuthChat and GuestChat components.

--- src/features/chatbot/AuthChat.test.tsx
+++ src/features/chatbot/AuthChat.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { AuthChat } from './AuthChat';
 
 // Mock ScottBot since we are testing layout
